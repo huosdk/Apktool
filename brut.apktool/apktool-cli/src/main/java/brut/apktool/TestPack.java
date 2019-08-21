92f35be14cbea3cba6fa965fea96728a6ea826d1
hongliang <lhl@huosdk.com>
2019-08-21 15:19:33 +0800

    解決控制台编译乱码问题

--- brut.apktool/apktool-cli/src/main/java/brut/apktool/TestPack.java
+++ brut.apktool/apktool-cli/src/main/java/brut/apktool/TestPack.java
@@ -9,7 +9,7 @@ public class TestPack {
     public static void main(String[] args) throws InterruptedException, BrutException, IOException {
 //        String cmd="b -f F:/union_sdk_new/game-packing-win32-x64-YunYu/core/workspace/game/maiyou/decompile -o F:/union_sdk_new/game-packing-win32-x64-YunYu/core/workspace/game/maiyou/output.apk";
 //        String cmd="d -f C:\\Users\\hongliang\\Desktop\\aiqu-2019031917.apk -o C:\\Users\\hongliang\\Desktop\\aiqu";
-        String cmd="b -f C:\\Users\\hongliang\\Desktop\\aiqu -o C:\\Users\\hongliang\\Desktop\\aiqu-2019031917_pack.apk";
+        String cmd="b -f H:\\union_pack\\game-packing-win32-x64-wanme\\core\\config\\sdk\\hisense\\demo -o H:\\union_pack\\game-packing-win32-x64-wanme\\core\\config\\sdk\\hisense\\demo_pack.apk";
         Main.main(cmd.split(" "));
 
     }
